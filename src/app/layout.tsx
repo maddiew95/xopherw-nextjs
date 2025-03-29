f5c9f10475f2cead090d8340bdbceb91ef3c4cc0
Christopher Wong <31772094+xopherw@users.noreply.github.com>
2025-03-30 08:27:20 +1100

    name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chris Wong",
+  title: "Maddie Wong",
   description: "An interactive site benerated by create-next-app",
 };
 
